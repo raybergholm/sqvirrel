2f8601ede7971e60af9fd0b3e7c624f38bade2ff
Ray Bergholm <raybergholm@gmail.com>
2018-06-15 16:08:10 +0300

    standardise superagent adapter methods

--- src/adapters/superagent.js
+++ src/adapters/superagent.js
@@ -1,73 +1,103 @@
-const request = require("superagent");
+const superagent = require("superagent");
 
-const _options = async ({ host, restPath, headers }) => {
+const _options = async ({ host, restPath, headers = null }) => {
     const url = restPath ? `${host}/${restPath}` : host;
 
-    return await request
-        .options(url)
-        .set(headers)
-        .then((response) => response)
-        .catch((error) => { throw error; });
+    const request = superagent.options(url);
+
+    if (headers) {
+        request.set(headers);
+    }
+
+    return await request;
 };
 
-const _get = async ({ host, restPath, query = null, additionalHeaders = {} }) => {
+const _get = async ({ host, restPath, headers = null, query = null }) => {
     const url = restPath ? `${host}/${restPath}` : host;
-    const headers = Object.assign({}, this.headers, additionalHeaders);
 
-    return request
-        .get(url)
-        .set(headers)
-        .query(query)
-        .then((response) => response)
-        .catch((error) => { throw error; });
+    const request = superagent.get(url);
+
+    if (headers) {
+        request.set(headers);
+    }
+
+    if (query) {
+        request.query(query);
+    }
+
+    return await request;
 };
 
-const _head = async ({ restPath, query = null, additionalHeaders = {} }) => {
+const _head = async ({ restPath, headers = null, query = null }) => {
     const url = restPath ? `${this.hostUrl}/${restPath}` : this.hostUrl;
-    const headers = Object.assign({}, this.headers, additionalHeaders);
 
-    return request
-        .head(url)
-        .set(headers)
-        .query(query)
-        .then((response) => response)
-        .catch((error) => { throw error; });
+    const request = superagent.head(url);
+
+    if (headers) {
+        request.set(headers);
+    }
+
+    if (query) {
+        request.query(query);
+    }
+
+    return await request;
 };
 
-const _post = async ({ restPath, body = "", additionalHeaders = {} }) => {
+const _post = async ({ restPath, headers = null, query = null, body = null }) => {
     const url = restPath ? `${this.hostUrl}/${restPath}` : this.hostUrl;
-    const headers = Object.assign({}, this.headers, additionalHeaders);
 
-    return request
-        .post(url)
-        .set(headers)
-        .send(body)
-        .then((response) => response)
-        .catch((error) => { throw error; });
+    const request = superagent.post(url);
+
+    if (headers) {
+        request.set(headers);
+    }
+
+    if (query) {
+        request.query(query);
+    }
+
+    if (body) {
+        request.send(body);
+    }
+
+    return await request;
 };
 
-const _put = async ({ restPath, body = "", additionalHeaders = {} }) => {
+const _put = async ({ restPath, headers = null, query = null, body = null }) => {
     const url = restPath ? `${this.hostUrl}/${restPath}` : this.hostUrl;
-    const headers = Object.assign({}, this.headers, additionalHeaders);
 
-    return request
-        .put(url)
-        .set(headers)
-        .send(body)
-        .then((response) => response)
-        .catch((error) => { throw error; });
+    const request = superagent.put(url);
+
+    if (headers) {
+        request.set(headers);
+    }
+
+    if (query) {
+        request.query(query);
+    }
+
+    if (body) {
+        request.send(body);
+    }
+
+    return await request;
 };
 
-const _delete = ({ restPath, query = null, additionalHeaders = {} }) => {
+const _delete = async ({ restPath, headers = null, query = null }) => {
     const url = restPath ? `${this.hostUrl}/${restPath}` : this.hostUrl;
-    const headers = Object.assign({}, this.headers, additionalHeaders);
 
-    return request
-        .delete(url)
-        .set(headers)
-        .query(query)
-        .then((response) => response)
-        .catch((error) => { throw error; });
+    const request = superagent.delete(url);
+
+    if (headers) {
+        request.set(headers);
+    }
+
+    if (query) {
+        request.query(query);
+    }
+
+    return await request;
 };
 
 
