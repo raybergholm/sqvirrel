ffaf47b8d831693a836aaa8f287c5bb714531f94
Ray Bergholm <raybergholm@gmail.com>
2018-06-15 15:49:51 +0300

    add util helpers

---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+// Allow a map of promises to be thenable after all promises have settled (resolve or reject)
+const settle = (promises) => Promise.all(
+    promises.map((promise) =>
+        Promise.resolve(promise).then(
+            (response) => ({ response }),
+            (error) => ({ error })
+        )
+    ));
+
+// Split array into two based on the predicate: true goes to the first element, false goes to the second
+const bifurcate = (arr, predicate) =>
+    arr.reduce((accumulator, value, index) => (accumulator[predicate(value, index) ? 0 : 1].push(value), accumulator), [[], []]);
+
+module.exports = {
+    settle,
+    bifurcate
+};
