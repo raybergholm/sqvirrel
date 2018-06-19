cef117d2009f7ce80028aebb85f3115e923fd133
Ray Bergholm <raybergholm@gmail.com>
2018-06-19 10:18:48 +0300

    update nodeHttpsRequest adapter to support passthrough

--- src/adapters/nodeHttpsRequest.js
+++ src/adapters/nodeHttpsRequest.js
@@ -2,6 +2,15 @@ const https = require("https");
 
 const DEFAULT_PORT = 443;
 
+const ALLOWED_METHODS = [
+    "OPTIONS",
+    "HEAD",
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE"
+];
+
 const createOptions = ({ host, path, method, port = DEFAULT_PORT, headers }) => ({
     hostname: host,
     port,
@@ -10,60 +19,53 @@ const createOptions = ({ host, path, method, port = DEFAULT_PORT, headers }) => 
     headers
 });
 
-const sendRequest = ({ host, port, method, restPath, headers = null, query = null, body = null }) => {
+const sendRequest = async ({ host, port, method, restPath, headers = null, query = null, body = null, ...passthrough }) => {
+    if (!method || ALLOWED_METHODS.indexOf(method) === -1) {
+        throw new Error(`Invalid HTTP method (should be one of: ${ALLOWED_METHODS.join(", ")})`);
+    }
+
     const path = query ? `${restPath}?${query}` : restPath;
     const options = createOptions({ host, port, path, method, headers });
 
-    return new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
         const successCallback = (response) => {
             let str = "";
             response.on("data", (chunk) => {
                 str += chunk;
             });
             response.on("end", () => {
-                resolve(str);
+                resolve({
+                    response: str,
+                    ...passthrough
+                });
             });
         };
 
-        const errorCallback = (err) => {
-            console.log("problem with request: " + err);
-            reject(err);
+        const errorCallback = (error) => {
+            reject({
+                error,
+                ...passthrough
+            });
         };
 
         const request = https.request(options, successCallback);
         request.on("error", errorCallback);
 
-        if (body) {
+        if (body && (method === "POST" || method === "PUT")) {
             request.write(body);
         }
         request.end();
     });
+
+    return await promise;
 };
 
-const _options = async ({ host, port, restPath, headers = null }) => {
-    return await sendRequest({ host, port, restPath, method: "OPTIONS", headers });
-};
-
-const _head = async ({ host, port, restPath, headers = null, query = null }) => {
-    return await sendRequest({ host, port, restPath, method: "HEAD", headers, query });
-};
-
-const _get = async ({ host, port, restPath, headers = null, query = null }) => {
-    return await sendRequest({ host, port, restPath, method: "GET", headers, query });
-};
-
-const _post = async ({ host, port, restPath, headers = null, query = null, body = null }) => {
-    return await sendRequest({ host, port, restPath, method: "POST", headers, query, body });
-};
-
-const _put = async ({ host, port, restPath, headers = null, query = null, body = null }) => {
-    return await sendRequest({ host, port, restPath, method: "PUT", headers, query, body });
-};
-
-const _delete = async ({ host, port, restPath, headers = null, query = null }) => {
-    return await sendRequest({ host, port, restPath, method: "DELETE", headers, query });
-};
-
+const _options = async (params) => await sendRequest({ ...params, method: "OPTIONS" });
+const _head = async (params) => await sendRequest({ ...params, method: "HEAD" });
+const _get = async (params) => await sendRequest({ ...params, method: "GET" });
+const _post = async (params) => await sendRequest({ ...params, method: "POST" });
+const _put = async (params) => await sendRequest({ ...params, method: "PUT" });
+const _delete = async (params) => await sendRequest({ ...params, method: "DELETE" });
 
 module.exports = {
     _options,
